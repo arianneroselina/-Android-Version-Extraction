16bd7c255f85be721112822d6e700352bdbb0994
arianneroselina <arianne.prananto@cosateq.com>
2022-12-08 00:18:25 +0100

    merge flutter and xamarin hash functions

--- src/main/hashing/main.py
+++ src/main/hashing/main.py
@@ -7,22 +7,22 @@ import subprocess
 
 flutter_file = 'libflutter.so'
 react_native_file = 'libreact*.so'
-xamarin_file = 'libxamarin-app.so'
+xamarin_file = 'libxa-internal-api.so'
 
 xamarin_folders = ['arm64-v8a', 'armeabi-v7a']
 flutter_folders = xamarin_folders + ['x86_64']
 react_native_folders = flutter_folders + ['x86']
 
 
-# get the hash of libflutter.so file from arm64_v8a, armeabi_v7a, and x86_64 folders, then write them in the
+# get the hash of $file file from the given $folders folders, then write them in the
 # corresponding .csv files.
-def hash_flutter(version, path):
+def hash_flutter_xamarin(version, path, framework_type, file, folders):
     path = add_slash_to_path(path)
 
-    for folder in flutter_folders:
-        csv_file = f'../../files/hashes/flutter/{folder}.csv'
+    for folder in folders:
+        csv_file = f'../../files/hashes/{framework_type}/{folder}.csv'
         try:
-            output = subprocess.check_output(one_file_command(path + folder + '\\' + flutter_file)).decode("utf-8")
+            output = subprocess.check_output(one_file_command(path + folder + '\\' + file)).decode("utf-8")
 
             start_index = output.find('so:') + 3
             end_index = output.find('CertUtil', start_index)
@@ -106,51 +106,6 @@ def hash_react_native(version, react_native_files):
                 raise RuntimeError("Command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
 
-# get the hash of libxamarin-app.so file from arm64_v8a and armeabi_v7a folders, then write them in the
-# corresponding .csv files.
-def hash_xamarin(version, path):
-    path = add_slash_to_path(path)
-
-    for folder in xamarin_folders:
-        csv_file = f'../../files/hashes/xamarin/{folder}.csv'
-        try:
-            output = subprocess.check_output(one_file_command(path + folder + '\\' + xamarin_file)).decode("utf-8")
-
-            start_index = output.find('so:') + 3
-            end_index = output.find('CertUtil', start_index)
-            output_hash = output[start_index + 1: end_index].strip()
-
-            finish = False
-            versions_list = []
-
-            if exists(csv_file):
-                with open(csv_file, 'r') as csv_read:
-                    csv_reader = csv.reader(csv_read)
-                    for row in csv_reader:
-                        change = False
-                        for i in range(len(row)):
-                            if change:
-                                row[i] = output_hash
-                                change = False
-                                finish = True
-                            if row[i] == version:
-                                change = True
-                        if len(row) != 0:
-                            versions_list.append(row)
-
-                with open(csv_file, 'w', newline='') as csv_write:
-                    writer = csv.writer(csv_write)
-                    writer.writerows(versions_list)
-
-            if not finish:
-                os.makedirs(os.path.dirname(csv_file), exist_ok=True)
-                with open(csv_file, 'a') as csv_append:
-                    csv_append.write(version + ',' + output_hash)
-
-        except subprocess.CalledProcessError as e:
-            raise RuntimeError("Command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
-
-
 def one_file_command(filename):
     return 'certutil -hashfile \"' + filename + '\" SHA256'
 
@@ -195,10 +150,10 @@ if __name__ == '__main__':
     path = add_slash_to_path(sys.argv[3])
 
     if sys.argv[1] == "flutter":
-        hash_flutter(sys.argv[2], sys.argv[3])
+        hash_flutter_xamarin(sys.argv[2], sys.argv[3], sys.argv[1], flutter_file, flutter_folders)
 
     if sys.argv[1] == "react_native":
         hash_react_native(sys.argv[2], glob.glob(path + react_native_folders[0] + '\\' + react_native_file))
 
     if sys.argv[1] == "xamarin":
-        hash_xamarin(sys.argv[2], sys.argv[3])
+        hash_flutter_xamarin(sys.argv[2], sys.argv[3], sys.argv[1], xamarin_file, xamarin_folders)
